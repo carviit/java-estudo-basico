53f2943ac3cce4e9e83c2fb60bce24604606bd3e
Carlos Vitor <carlosfdm@outlook.com>
2022-11-04 19:56:09 -0300

    .

---
+++ java-testa-valores/src/ComandosRepeticao.java
@@ -0,0 +1,30 @@
+public class ComandosRepeticao{
+
+    public static void main(String[] args){
+
+        System.out.println("Testando Condicionais");
+
+        int idade = 15;
+        int quantidadePessoas = 3;
+
+        if (idade >= 15 && idade <= 18) {
+
+            System.out.println("Você ganhou 10% de desconto, pois você possui " + idade + " anos.");
+
+        } else if(quantidadePessoas >= 2) {
+
+            System.out.println("Você ganhou 10% de desconto");
+
+        } else{
+
+            System.out.println("Você ganhou 10% de desconto");
+        }
+
+
+
+        
+
+    }       
+
+}
+
