9fa1cad49d871509d79db65d5f0a8bab74a3b403
Carlos Vitor <carlosfdm@outlook.com>
2022-11-04 20:58:39 -0300

    .

---
+++ java-escopo/java-repeticoes/src/LacosRepeticoes.java
@@ -0,0 +1,21 @@
+public class LacosRepeticoes {
+    
+    public static void main(String[] args) throws Exception {
+
+        int numero = 0;
+
+        while(numero <= 10){
+
+            System.out.println(numero);
+
+            numero++;
+        }
+
+        for(int i = 0;i<=50;i+=5){
+
+            System.out.println(i);
+
+        }
+
+    }
+}
